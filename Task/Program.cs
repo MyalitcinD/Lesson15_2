79d136d9f97d56ec0a94511dcbfee28399eb1ed6
Денис <mden1996.07@gmail.com>
2024-03-14 02:19:51 +0300

    done

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,7 +10,28 @@ namespace Task
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите начальное число и шаг для ариф прогресии и геом прогресии");
+            Console.Write("начальное значение = ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("шаг/множитель = ");
+            int step = Convert.ToInt32(Console.ReadLine());
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.SetStart(start);
+            arithProgression.SetStep(step);
+            GeomProgression geomProgression = new GeomProgression();
+            geomProgression.SetStart(start);
+            geomProgression.SetCommRatio(step);
 
+            Console.WriteLine($"Члены арифметической прогресии = {arithProgression.GetNext()} {arithProgression.GetNext()} {arithProgression.GetNext()} {arithProgression.GetNext()} {arithProgression.GetNext()}");
+            Console.WriteLine($"Члены геометричекой прогресии = {geomProgression.GetNext()} {geomProgression.GetNext()} {geomProgression.GetNext()} {geomProgression.GetNext()} {geomProgression.GetNext()}");
+            //Console.WriteLine($"start = {arithProgression.} current = {}");
+            arithProgression.Reset();
+            geomProgression.Reset();
+            Console.WriteLine();
+            Console.WriteLine($"Члены арифметической прогресии = {arithProgression.GetNext()} {arithProgression.GetNext()} {arithProgression.GetNext()} {arithProgression.GetNext()} {arithProgression.GetNext()}");
+            Console.WriteLine($"Члены геометричекой прогресии = {geomProgression.GetNext()} {geomProgression.GetNext()} {geomProgression.GetNext()} {geomProgression.GetNext()} {geomProgression.GetNext()}");
+
+            Console.ReadKey();
 
         }
     }
---
+++ Task/Series.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int start;
+        int step;
+        int curr;
+        public ArithProgression() { }
+        public ArithProgression(int startvalue, int stepValue)
+        {
+            start = startvalue;
+            step = stepValue;
+            curr = startvalue;
+        }
+        public int GetNext()
+        {
+            curr += step;
+            return curr;
+        }
+
+        public void Reset()
+        {
+            curr = start;
+        }
+
+        public void SetStart(int x)
+        {
+            start = x;
+            curr = x;
+        }
+        public void SetStep(int x)
+        {
+            step = x;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        int start;
+        int commRatio;
+        int curr;
+        public GeomProgression() { }
+        public GeomProgression(int StartValue, int CommonRatio)
+        {
+            start = StartValue;
+            curr = StartValue;
+            commRatio = CommonRatio;
+        }
+        public int GetNext()
+        {
+            curr = curr * commRatio;
+            return curr;
+        }
+
+        public void Reset()
+        {
+            curr = start;
+        }
+
+        public void SetStart(int x)
+        {
+            start = x;
+            curr = x;
+        }
+        public void SetCommRatio(int x)
+        {
+            commRatio = x;
+        }
+    }
+}
